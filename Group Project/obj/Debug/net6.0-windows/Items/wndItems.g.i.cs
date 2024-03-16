2118ad05e37a39609c39ba4926fc8cc34c2cbf8f
michaelanderson22 <michaelanderson22@mail.weber.edu>
2024-03-16 17:28:11 -0600

    starting files 3
    added oleDB dependency

--- Group Project/obj/Debug/net6.0-windows/Items/wndItems.g.i.cs
+++ Group Project/obj/Debug/net6.0-windows/Items/wndItems.g.i.cs
@@ -53,7 +53,7 @@ namespace Group_Project {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Group Project;component/items/wnditems.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Group Project;V1.0.0.0;component/items/wnditems.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Items\wndItems.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
