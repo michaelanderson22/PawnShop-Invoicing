285ce898f8f173e1c44fb08b7d1af7323dd675b0
belloon84 <belloon84@gmail.com>
2024-03-16 18:47:00 -0600

    for fun

--- Group Project/obj/Debug/net6.0-windows/Group Project.AssemblyInfo.cs
+++ Group Project/obj/Debug/net6.0-windows/Group Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d8997a184f96ab712bbe006e6e7d91647e850e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
