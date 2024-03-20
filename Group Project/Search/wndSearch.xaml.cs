9b4f311e99adef9f000ce221932a51612315dcaa
belloon84 <89543664+belloon84@users.noreply.github.com>
2024-03-20 12:54:57 -0600

    Update wndSearch.xaml.cs

--- Group Project/Search/wndSearch.xaml.cs
+++ Group Project/Search/wndSearch.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Group_Project
-{
+namespace Group_Project {
     /// <summary>
     /// Interaction logic for wndSearch.xaml
     /// </summary>
-    public partial class wndSearch : Window
-    {
-        public wndSearch()
-        {
+    public partial class wndSearch : Window {
+        clsSearchLogic searchLogic = new clsSearchLogic();
+        public wndSearch() {
             InitializeComponent();
+            populateThings();
+        }
+
+        private void populateThings() {
+            dgDataGrid.ItemsSource = searchLogic.getInvoices();
+            cbNumber.ItemsSource = searchLogic.getNumbers();
+            cbDate.ItemsSource = searchLogic.getDates();
+            cbCosts.ItemsSource = searchLogic.getCosts();
+        }
+
+        //property for invoiceNum
+
+        private void btnClear_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e) {
+            //set the invoice ID local variable
         }
     }
 }
