b489cb6443b24c4fcfece4fb968a6cb47441862c
michaelanderson22 <michaelanderson22@mail.weber.edu>
2024-03-25 18:13:18 -0600

    Revert "commit meta files"
    This reverts commit 6dcd6be12c45366b6653434246c968071b25e228.

--- Group Project/obj/Debug/net6.0-windows/Main/wndMain.g.i.cs
+++ Group Project/obj/Debug/net6.0-windows/Main/wndMain.g.i.cs
@@ -109,7 +109,7 @@ namespace Group_Project {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Group Project;V1.0.0.0;component/main/wndmain.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Group Project;component/main/wndmain.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Main\wndMain.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
