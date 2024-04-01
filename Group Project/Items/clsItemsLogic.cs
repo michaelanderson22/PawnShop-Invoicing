797e696910cfdd4779d1d03d24e0d98e6d1ecc51
michaelanderson22 <michaelanderson22@mail.weber.edu>
2024-04-01 15:07:13 -0600

    additional functionality for the main window

--- Group Project/Items/clsItemsLogic.cs
+++ Group Project/Items/clsItemsLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,31 +11,40 @@ namespace Group_Project
     {
 
         //!!! <<<<****>>>>> This list will be public and static for any window that needs full list of items to use
-        //public List<clsItem> getItemList()
-        //{
-        //    int retVal = 0;
+        public static List<clsItem> getItemList()
+        {
+            /// <summary>
+            /// clsDataAccess object. Handles all database calls.
+            /// </summary>
+            clsDataAccess db = new clsDataAccess();
 
-        //    DataSet ds = new DataSet();
-        //    List<clsItem> itemList = new List<clsItem>();
+            /// <summary>
+            /// Holds the sql statement to be executed by clsDataAccess.
+            /// </summary>
+            string sql = "";
 
-        //    sql = clsMainSQL.getItemDesc();
-        //    ds = db.ExecuteSQLStatement(sql, ref retVal);
+            int retVal = 0;
 
-        //    if (ds != null && ds.Tables.Count > 0)
-        //    {
-        //        foreach (DataRow row in ds.Tables[0].Rows)
-        //        {
-        //            clsItem item = new clsItem();
-        //            item.itemCode = row["ItemCode"].ToString();
-        //            item.itemDesc = row["ItemDesc"].ToString();
-        //            item.itemCost = decimal.Parse(row["Cost"].ToString());
+            DataSet ds = new DataSet();
+            List<clsItem> itemList = new List<clsItem>();
 
-        //            itemList.Add(item);
-        //        }
-        //    }
-        //    return itemList;
+            sql = clsMainSQL.getItemDesc();
+            ds = db.ExecuteSQLStatement(sql, ref retVal);
 
-        //}
+            if (ds != null && ds.Tables.Count > 0)
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    clsItem item = new clsItem();
+                    item.itemCode = row["ItemCode"].ToString();
+                    item.itemDesc = row["ItemDesc"].ToString();
+                    item.itemCost = decimal.Parse(row["Cost"].ToString());
+
+                    itemList.Add(item);
+                }
+            }
+            return itemList;
+        }
     }
 
     public class clsItem
@@ -43,6 +53,11 @@ namespace Group_Project
         public string itemCode { get; set; }
         public string itemDesc { get; set; }
         public decimal itemCost { get; set; }
+
+        public override string ToString()
+        {
+            return itemDesc;
+        }
 
     }
 }
