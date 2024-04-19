345ca446f68ca8824035855d00d2d8da2de9a727
Mitch Tryon <mitchtryon@yahoo.com>
2024-04-18 23:01:13 -0600

    Items Window functionality
    Added last of submit button/error checking. Protection for deletion of items on an invoice, and warning labels. Try Catch implementation started but not finished, some xml notes, need to finish notes.

--- Group Project/Items/clsItemsSQL.cs
+++ Group Project/Items/clsItemsSQL.cs
@@ -2,63 +2,89 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Group_Project
-{
-    public class clsItemsSQL
-    {
-        /// <summary>
-        /// This method returns a string for a SQL query to grab all records from the ItemDesc table
-        /// </summary>
-        /// <returns> A string SQL query</returns>
-        public string QueryAllItemDesc()
-        {
-            return "select ItemCode, ItemDesc, Cost from ItemDesc;";
-        }
-        /// <summary>
-        /// This method returns a string for a SQL query to find a unique invoice number from the ItemDesc table using an item code
-        /// </summary>
-        /// <param name="itemCode">Integer, corresponds to the item in the itemDesc Table</param>
-        /// <returns> A string SQL query</returns>
-        public string QueryInvoiceByItemCode(string itemCode)
-        {
-            return $"select distinct(InvoiceNum) from LineItems where ItemCode = '{itemCode}';";
-        }
-        /// <summary>
-        /// This method returns a string for a SQL query to update an items description and cost by its itemcode
-        /// </summary>
-        /// <param name="desc"> String, description of item</param>
-        /// <param name="cost"> Integer, Cost of Item</param>
-        /// <param name="itemCode"> Int, item code for item in itemDesc table</param>
-        /// <returns> A string SQL query</returns>
-        public string UpdateItemDescAndCost(string desc, int cost, string itemCode)
-        {
-            return $"Update ItemDesc Set ItemDesc = '{desc}', Cost = {cost} where ItemCode = '{itemCode}';";
+namespace Group_Project {
+	public class clsItemsSQL {
+		/// <summary>
+		/// This method returns a string for a SQL query to grab all records from the ItemDesc table
+		/// </summary>
+		/// <returns> A string SQL query</returns>
+		public string QueryAllItemDesc() {
+			try {
+				return "select ItemCode, ItemDesc, Cost from ItemDesc;";
+			}
+			catch (Exception ex) {
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+								   MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+			}
+		}
+		/// <summary>
+		/// This method returns a string for a SQL query to find a unique invoice number from the ItemDesc table using an item code
+		/// </summary>
+		/// <param name="itemCode">Integer, corresponds to the item in the itemDesc Table</param>
+		/// <returns> A string SQL query</returns>
+		public string QueryInvoiceByItemCode(string itemCode) {
+			try {
+				return $"select distinct(InvoiceNum) from LineItems where ItemCode = '{itemCode}';";
+			}
+			catch (Exception ex) {
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+								   MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+			}
+		}
+		/// <summary>
+		/// This method returns a string for a SQL query to update an items description and cost by its itemcode
+		/// </summary>
+		/// <param name="desc"> String, description of item</param>
+		/// <param name="cost"> Integer, Cost of Item</param>
+		/// <param name="itemCode"> Int, item code for item in itemDesc table</param>
+		/// <returns> A string SQL query</returns>
+		public string UpdateItemDescAndCost(string desc, int cost, string itemCode) {
+			try {
+				return $"Update ItemDesc Set ItemDesc = '{desc}', Cost = {cost} where ItemCode = '{itemCode}';";
 
-        }
-        /// <summary>
-        /// This method returns a string for a SQL query to insert a new record into the ItemDesc table
-        /// </summary>
-        /// <param name="desc"> String, description of item</param>
-        /// <param name="cost"> Integer, Cost of Item</param>
-        /// <param name="itemCode"> Int, item code for item in itemDesc table</param>
-        /// <returns> A string SQL query</returns>
-        public string InsertNewItem(string desc, string itemCode, int cost)
-        {
-            return $"Insert into ItemDesc(ItemCode, ItemDesc, Cost) Values('{itemCode}', '{desc}', {cost});";
-        }
-        /// <summary>
-        /// This method returns a string for a SQL query to a record from the ItemDesc table
-        /// </summary>
-        /// <param name="itemCode">Integer, item to be deleted</param>
-        /// <returns> A string SQL query</returns>
-        public string DeleteFromItemDesc(string itemCode)
-        {
-            return $"Delete from ItemDesc Where ItemCode = '{itemCode}';";
-        }
+			}
+			catch (Exception ex) {
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+								   MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+			}
 
-    }
+		}
+		/// <summary>
+		/// This method returns a string for a SQL query to insert a new record into the ItemDesc table
+		/// </summary>
+		/// <param name="desc"> String, description of item</param>
+		/// <param name="cost"> Integer, Cost of Item</param>
+		/// <param name="itemCode"> Int, item code for item in itemDesc table</param>
+		/// <returns> A string SQL query</returns>
+		public string InsertNewItem(string desc, string itemCode, int cost) {
+			try {
+				return $"Insert into ItemDesc(ItemCode, ItemDesc, Cost) Values('{itemCode}', '{desc}', {cost});";
+
+			}
+			catch (Exception ex) {
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+								   MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+			}
+		}
+		/// <summary>
+		/// This method returns a string for a SQL query to a record from the ItemDesc table
+		/// </summary>
+		/// <param name="itemCode">Integer, item to be deleted</param>
+		/// <returns> A string SQL query</returns>
+		public string DeleteFromItemDesc(string itemCode) {
+			try {
+				return $"Delete from ItemDesc Where ItemCode = '{itemCode}';";
+			}
+			catch (Exception ex) {
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+								   MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+			}
+		}
+
+	}
 }
--- Group Project/Items/wndItems.xaml.cs
+++ Group Project/Items/wndItems.xaml.cs
@@ -19,6 +19,8 @@ namespace Group_Project {
 	/// Interaction logic for Window1.xaml
 	/// </summary>
 	public partial class wndItems : Window {
+		#region Attributes
+
 		/// <summary>
 		/// clsItemsSQL obj to use for SQL strings
 		/// </summary>
@@ -44,16 +46,36 @@ namespace Group_Project {
 		/// </summary>
 		int cost;
 		/// <summary>
+		/// Enum for tracking state of radio buttons
+		/// </summary>
+		public enum RadioButtons {
+			None,
+			Add,
+			Edit,
+			Delete
+		}
+		RadioButtons eRadioButtons;
+		#endregion
+
+		/// <summary>
 		/// Constructor, runs when window is initialized
 		/// </summary>
 		public wndItems() {
-			InitializeComponent();
-			clsItemsSQL = new clsItemsSQL();
-			data = new clsDataAccess();
-			DataSet ds = new DataSet();
-			sql = desc = itemCode = "";
-			cost = 0;
-			RefreshItemsGrid();
+			try {
+				InitializeComponent();
+				clsItemsSQL = new clsItemsSQL();
+				data = new clsDataAccess();
+				DataSet ds = new DataSet();
+				sql = desc = itemCode = "";
+				cost = 0;
+				WarningLabel.Content = "";
+				eRadioButtons = RadioButtons.None;
+				RefreshItemsGrid();
+			}
+			catch (Exception) {
+
+				throw;
+			}
 		}
 		/// <summary>
 		/// This method is called when the ItemGrid needs to have its DataSet Refreshed
@@ -61,6 +83,7 @@ namespace Group_Project {
 		private void RefreshItemsGrid() {
 			try {
 				ItemGrid.ItemsSource = new DataView(clsItemsLogic.getItemDataSet().Tables[0]);
+				//				SubmitButton.IsEnabled = false;
 			}
 			catch (Exception ex) {
 				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
@@ -78,16 +101,18 @@ namespace Group_Project {
 				Int32.TryParse(costTextBox.Text, out cost);
 				desc = descTextBox.Text;
 				itemCode = codeTextBox.Text;
-				if (AddRdoBtn.IsChecked == true && clsItemsLogic.IsCodeUnique(itemCode)) {
-					if (codeTextBox.Text != "" && costTextBox.Text != "" && descTextBox.Text != "") {
+				if (Add.IsChecked == true) {
+					if (clsItemsLogic.IsCodeUnique(itemCode) && codeTextBox.Text != "" && costTextBox.Text != "" && descTextBox.Text != "") {
 						sql = clsItemsSQL.InsertNewItem(desc, itemCode, cost);
 						data.ExecuteNonQuery(sql);
 					}
 					else {
+						WarningLabel.Content = "Check Item to Add, must have unique Code and all values filled.";
+						codeTextBox.Focus();
 						return;
 					}
 				}
-				else if (EditRdoBtn.IsChecked == true && !clsItemsLogic.IsCodeUnique(itemCode)) {
+				else if (Edit.IsChecked == true && !clsItemsLogic.IsCodeUnique(itemCode)) {
 					if (codeTextBox.Text != "" && costTextBox.Text != "" && descTextBox.Text != "") {
 						sql = clsItemsSQL.UpdateItemDescAndCost(desc, cost, itemCode);
 						data.ExecuteNonQuery(sql);
@@ -96,10 +121,15 @@ namespace Group_Project {
 						return;
 					}
 				}
-				else if (DeleteRdoBtn.IsChecked == true && !clsItemsLogic.IsCodeUnique(itemCode)) {
+				else if (Delete.IsChecked == true && !clsItemsLogic.IsCodeUnique(itemCode)) {
 					if (codeTextBox.Text != "") {
-						sql = clsItemsSQL.DeleteFromItemDesc(itemCode);
-						data.ExecuteNonQuery(sql);
+						if (clsItemsLogic.IsCodeSafeToDelete(itemCode)) {
+							sql = clsItemsSQL.DeleteFromItemDesc(itemCode);
+							data.ExecuteNonQuery(sql);
+						}
+						else {
+							WarningLabel.Content = "Can Not Delete Item, Exists in an invoice!";
+						}
 					}
 					else {
 						return;
@@ -110,7 +140,8 @@ namespace Group_Project {
 				}
 				RefreshItemsGrid();
 				codeTextBox.Text = costTextBox.Text = descTextBox.Text = "";
-				DeleteRdoBtn.IsChecked = EditRdoBtn.IsChecked = AddRdoBtn.IsChecked = false;
+				SubmitButton.IsEnabled = false;
+				Delete.IsChecked = Edit.IsChecked = Add.IsChecked = false;
 			}
 			catch (Exception ex) {
 				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
@@ -169,7 +200,7 @@ namespace Group_Project {
 			}
 		}
 		/// <summary>
-		/// This meethod runs when the descTextBox has a keydown event
+		/// This method runs when the descTextBox has a keydown event
 		/// </summary>
 		/// <param name="sender">The desctextbox</param>
 		/// <param name="e">Keypress event</param>
@@ -184,41 +215,103 @@ namespace Group_Project {
 								   MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
 			}
 		}
+		/// <summary>
+		/// This method is passed the event from a keypress and a bool for if it is checking for an int or not, handles unwanted key events
+		/// </summary>
+		/// <param name="inputIsInt">Bool for if int is desired input</param>
+		/// <param name="e"></param>
 		private void CheckInput(bool inputIsInt, KeyEventArgs e) {
-			if (inputIsInt) {
-				//0 - 9 nums and numpad
-				if ((e.Key >= Key.D0 && e.Key <= Key.D9) ||
-					(e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)) {
+			try {
+				if (inputIsInt) {
+					//0 - 9 nums and numpad
+					if ((e.Key >= Key.D0 && e.Key <= Key.D9) ||
+						(e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)) {
+					}
+					// allow deletion
+					else if (e.Key == Key.Delete) {
+					}
+					// if enter, submit
+					else if (e.Key == Key.Enter) {
+					}
+					// disallow any unwanted keypress
+					else {
+						e.Handled = true;
+					}
 				}
-				// allow deletion
-				else if (e.Key == Key.Delete) {
-				}
-				// if enter, submit
-				else if (e.Key == Key.Enter) {
-				}
-				// disallow any unwanted keypress
 				else {
-					e.Handled = true;
+					// alpha chars
+					if (e.Key >= Key.A && e.Key <= Key.Z) {
+					}
+					//allow deletion
+					else if (e.Key == Key.Delete) {
+					}
+					// disallow unwanted keypress events
+					else {
+						e.Handled = true;
+					}
 				}
-			}	
-			else {
-				// alpha chars
-				if (e.Key >= Key.A && e.Key <= Key.Z) {
-				}
-				//allow deletion
-				else if (e.Key == Key.Delete) {
-				}			
-				// disallow unwanted keypress events
-				else {
-					e.Handled = true;
-				}
+			}
+			catch (Exception) {
+
+				throw;
 			}
 		}
 		/// <summary>
 		/// This method will check for radio buttons and text fields content, enabling Submit button if appropriate
 		/// </summary>
-		public void ToggleSubmitEnabled() {
-			
+		public void ToggleSubmitEnabled(Enum selectedRdoBtn) {
+			try {
+				if (eRadioButtons.Equals(RadioButtons.Add)) {
+					if (codeTextBox.Text != "" && costTextBox.Text != "" && descTextBox.Text != "") {
+						SubmitButton.IsEnabled = true;
+					}
+				}
+				else if (eRadioButtons.Equals(RadioButtons.Edit)) {
+					if (codeTextBox.Text != "" && costTextBox.Text != "" && descTextBox.Text != "") {
+						SubmitButton.IsEnabled = true;
+					}
+				}
+				else if (eRadioButtons.Equals(RadioButtons.Delete)) {
+					if (codeTextBox.Text != "") {
+						SubmitButton.IsEnabled = true;
+					}
+				}
+				else {
+					SubmitButton.IsEnabled = false;
+					return;
+				}
+			}
+			catch (Exception) {
+
+				throw;
+			}
+		}
+		/// <summary>
+		/// When radio button is checked, this method clears teh WarningLabel Label in the items window
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void RdoBtn_Checked(object sender, RoutedEventArgs e) {
+			try {
+				RadioButton rdo = sender as RadioButton;
+				WarningLabel.Content = "";
+				switch (rdo.Name) {
+					case "Add":
+						eRadioButtons = RadioButtons.Add;
+						break;
+					case "Edit":
+						eRadioButtons = RadioButtons.Edit;
+						break;
+					case "Delete":
+						eRadioButtons = RadioButtons.Delete;
+						break;
+				}
+				ToggleSubmitEnabled(eRadioButtons);
+			}
+			catch (Exception) {
+
+				throw;
+			}
 		}
 	}
 }
